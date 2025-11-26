60a1cee308e9b6c6d201a9cdcd187b6f750fbd82
Sean Blizard <emailseanblizard@gmail.com>
2025-11-26 17:31:17 -0500

    aggergate sigs

--- BLS-signature/main.go
+++ BLS-signature/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	sigagg "bls-signature/signature-aggergations"
+)
 
 func main() {
 	fmt.Println("Running BLS demo test suite...")
@@ -44,6 +48,24 @@ func main() {
 		fmt.Println("  PASS")
 	}
 
+	fmt.Println("TestVerifyWrongSignature:")
+	wrongSig := Sign(out2.SecretKey, msg)
+	if Verifiy(*out.PublicKey, wrongSig, msg) {
+		panic("TestVerifyWrongSignature FAILED")
+	} else {
+		fmt.Println("  PASS")
+	}
+
+	fmt.Println("TestMultipleMessages:")
+	messages := []string{"message1", "message2", "message3"}
+	for i, m := range messages {
+		testSig := Sign(out.SecretKey, m)
+		if !Verifiy(*out.PublicKey, testSig, m) {
+			panic("TestMultipleMessages FAILED: signature " + fmt.Sprint(i) + " invalid")
+		}
+	}
+	fmt.Println("  PASS")
+
 	fmt.Println("TestFullBLSFlow:")
 	msg2 := "integration test"
 	sig2 := Sign(out.SecretKey, msg2)
@@ -56,5 +78,65 @@ func main() {
 	}
 	fmt.Println("  PASS")
 
-	fmt.Println("All tests PASSED")
+	fmt.Println("\nTestAggregateSignatures:")
+	keyPair1, err := keyGen()
+	if err != nil {
+		panic("Failed to generate keypair 1")
+	}
+	keyPair2, err := keyGen()
+	if err != nil {
+		panic("Failed to generate keypair 2")
+	}
+
+	commonMsg := "aggregate test message"
+	sigOne := Sign(keyPair1.SecretKey, commonMsg)
+	sigTwo := Sign(keyPair2.SecretKey, commonMsg)
+
+	aggSig, err := sigagg.AggregateSignatures(sigOne, sigTwo, keyPair1.PublicKey, keyPair2.PublicKey)
+	if err != nil {
+		panic("TestAggregateSignatures FAILED: " + err.Error())
+	}
+	if aggSig == nil {
+		panic("TestAggregateSignatures FAILED: aggregate signature is nil")
+	}
+
+	if !sigagg.AggergateVerifiy(keyPair1.PublicKey, keyPair2.PublicKey, aggSig, []byte(commonMsg)) {
+		panic("TestAggregateSignatures FAILED: verification failed")
+	}
+	fmt.Println("  PASS")
+
+	fmt.Println("\nTestAggregateWrongMessage:")
+	wrongMsg := "wrong aggregate message"
+	if sigagg.AggergateVerifiy(keyPair1.PublicKey, keyPair2.PublicKey, aggSig, []byte(wrongMsg)) {
+		panic("TestAggregateWrongMessage FAILED: wrong message accepted")
+	}
+	fmt.Println("  PASS")
+
+	fmt.Println("\nTestAggregateSwappedKeys:")
+	if sigagg.AggergateVerifiy(keyPair2.PublicKey, keyPair1.PublicKey, aggSig, []byte(commonMsg)) {
+		panic("TestAggregateSwappedKeys FAILED: swapped keys accepted")
+	}
+	fmt.Println("  PASS")
+
+	fmt.Println("\nTestMultipleAggregations:")
+	keyPair3, _ := keyGen()
+	keyPair4, _ := keyGen()
+	sharedMsg := "shared message for aggregation"
+	sig3 := Sign(keyPair3.SecretKey, sharedMsg)
+	sig4 := Sign(keyPair4.SecretKey, sharedMsg)
+
+	aggSig2, err := sigagg.AggregateSignatures(sig3, sig4, keyPair3.PublicKey, keyPair4.PublicKey)
+	if err != nil {
+		panic("TestMultipleAggregations FAILED: " + err.Error())
+	}
+
+	if !sigagg.AggergateVerifiy(keyPair1.PublicKey, keyPair2.PublicKey, aggSig, []byte(commonMsg)) {
+		panic("TestMultipleAggregations FAILED: first aggregation invalid")
+	}
+	if !sigagg.AggergateVerifiy(keyPair3.PublicKey, keyPair4.PublicKey, aggSig2, []byte(sharedMsg)) {
+		panic("TestMultipleAggregations FAILED: second aggregation invalid")
+	}
+	fmt.Println("  PASS")
+
+	fmt.Println("\nAll tests PASSED")
 }
---
+++ BLS-signature/signature-aggergations/aggergate.go
@@ -0,0 +1,64 @@
+package signatureaggergations
+
+import (
+	"errors"
+
+	blst "github.com/supranational/blst/bindings/go"
+)
+
+var AGG_DST = []byte("AGGREGATE_SIGNATURE_DST")
+var DST = []byte("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
+
+func AggregateSignatures(sigA, sigB *blst.P2Affine, pkA, pkB *blst.P1Affine) (*blst.P2Affine, error) {
+	t := ComputeHash(pkA, pkB, AGG_DST)
+
+	sigAt := new(blst.P2)
+	sigAt.FromAffine(sigA)
+	sigAt.MultAssign(t)
+
+	agg := new(blst.P2Aggregate)
+	if !agg.Add(sigAt.ToAffine(), true) {
+		return nil, errors.New("failed to add sigAt")
+	}
+	if !agg.Add(sigB, true) {
+		return nil, errors.New("failed to add sigB")
+	}
+
+	return agg.ToAffine(), nil
+}
+
+func AggergateVerifiy(pkA, pkB *blst.P1Affine, aggSig *blst.P2Affine, msg []byte) bool {
+	t := ComputeHash(pkA, pkB, AGG_DST)
+
+	pkAt := new(blst.P1)
+	pkAt.FromAffine(pkA)
+	pkAt.MultAssign(t)
+
+	pkAgg := new(blst.P1Aggregate)
+	if !pkAgg.Add(pkAt.ToAffine(), true) {
+		return false
+	}
+	if !pkAgg.Add(pkB, true) {
+		return false
+	}
+	pkAggAffine := pkAgg.ToAffine()
+
+	return aggSig.Verify(
+		true,
+		pkAggAffine,
+		true,
+		msg,
+		DST,
+	)
+}
+
+func ComputeHash(pkA, pkB *blst.P1Affine, dst []byte) *blst.Scalar {
+	pkABytes := pkA.Compress()
+	pkBBytes := pkB.Compress()
+
+	message := append(pkABytes, pkBBytes...)
+
+	t := blst.HashToScalar(message, dst)
+
+	return t
+}
